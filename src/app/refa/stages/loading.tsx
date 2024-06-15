79d6b82b4b0c8df60951cdac39655ba6c2fc8cd9
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-15 16:31:49 +0200

    💄 chore: update skeleton loading ui

--- src/app/refa/stages/loading.tsx
+++ src/app/refa/stages/loading.tsx
@@ -4,8 +4,8 @@ export default function Loading() {
     return (
         <div className='relative bg-white w-full min-h-full rounded-lg p-8'>
             <div className='flex flex-wrap gap-4 min-w-full'>
-                {Array.from({ length: 5 }).map((_, i) => (
-                    <Skeleton key={i} className="h-32 w-64 m-2" />
+                {Array.from({ length: 7 }).map((_, i) => (
+                    <Skeleton key={i} className="h-36 w-72 m-2" />
                 ))}
             </div>
         </div>
