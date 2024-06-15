b7db56d7e3f75dc7fa4682c9cd363ffff2b8d72e
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-15 15:15:10 +0200

    ✨ feat(API): add RefA router

---
+++ src/server/api/routers/refa.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+export const refaRouter = createTRPCRouter({
+  stages: publicProcedure
+    .query(({ input, ctx }) => {
+        return ctx.db.stage.findMany({
+            orderBy: { stageNumber: "asc" },
+        });
+    }),
+  areasByStage: publicProcedure
+    .input(
+      z.object({
+        stageNumber: z.number(),
+      })
+    )
+    .query(({ input, ctx }) => {
+      return ctx.db.stage.findFirst({
+        where: {
+          stageNumber: input.stageNumber,
+        },
+        include: {
+          areas: true,
+        },
+      });
+    }),
+});
