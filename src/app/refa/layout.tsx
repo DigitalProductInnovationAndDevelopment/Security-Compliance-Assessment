aff66f194d2b49b5a1c228057fed5ce98405646a
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-06-15 15:14:53 +0200

    ✨ feat: add UI layout for refa page

---
+++ src/app/refa/layout.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import { Button } from '~/components/ui/button'
+import { api } from '~/trpc/server'
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const stages = await api.refa.stages()
+
+  return (
+    <div className='relative w-full px-6'>
+      <div className='flex min-w-full -ml-3'>
+        {stages.map((stage) => (
+          <div key={stage.stage} className='border-gray-200 py-4'>
+            <div className='flex flex-row'>
+              <span className='inline-flex items-center'>
+                <Link href={'/refa/stages/' + stage.stageNumber}>
+                  <Button variant={'link'} className='text-md font-medium' >{stage.stage}</Button>
+                </Link>
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+      {children}
+    </div>
+  )
+}
