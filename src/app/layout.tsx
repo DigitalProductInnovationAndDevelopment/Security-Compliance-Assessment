6893da255101fc6b1cad8f01adac08e7359840c4
Nataliia Severynenko <49091101+severynenko@users.noreply.github.com>
2024-06-28 00:33:33 +0200

    Revert "Install Radix Themes"
    This reverts commit 7c19419792a6afa5179580305e6e536a2edb1f08.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
 import "~/styles/globals.css";
-import "@radix-ui/themes/styles.css";
 
 import { GeistSans } from "geist/font/sans";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { DM_Sans as FontSans } from "next/font/google";
+import { DM_Sans as FontSans } from "next/font/google"
 import { cn } from "~/lib/utils";
 import Header from "~/components/header";
 import { NextAuthProvider } from "./providers/NextAuthProvider";
 import { get } from "http";
 import { getServerAuthSession } from "~/server/auth";
-import { Theme } from "@radix-ui/themes";
+
 
 export const metadata = {
   title: "Security Compliance Assessment",
-  description:
-    "Siemens Security Compliance Assessment Tool — Digitial Product Innovation",
+  description: "Siemens Security Compliance Assessment Tool — Digitial Product Innovation",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-});
+})
 
 export default async function RootLayout({
   children,
@@ -35,17 +33,15 @@ export default async function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
-        <Theme>
-          <NextAuthProvider>
-            <TRPCReactProvider>
-              <Header session={session} />
-              {children}
-            </TRPCReactProvider>
-          </NextAuthProvider>
-        </Theme>
+        <NextAuthProvider>
+          <TRPCReactProvider>
+            <Header session={session} />
+            {children}
+          </TRPCReactProvider>
+        </NextAuthProvider>
       </body>
     </html>
   );
