9e1b48684b8285fbc3a0ded6c91d37ec7cfc17bd
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-07-01 14:15:58 +0100

    ♻️ chore: refactor code

--- src/app/refa/layout.tsx
+++ src/app/refa/layout.tsx
@@ -10,11 +10,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <div className='relative w-full px-6'>
       <div className='flex min-w-full -ml-3'>
         {stages.map((stage) => (
-          <div key={stage.stage} className='border-gray-200 py-4'>
+          <div key={stage.name} className='border-gray-200 py-4'>
             <div className='flex flex-row'>
               <span className='inline-flex items-center'>
                 <Link href={'/refa/stages/' + stage.stageNumber}>
-                  <Button variant={'link'} className='text-md font-medium' >{stage.stage}</Button>
+                  <Button variant={'link'} className='text-md font-medium' >{stage.name}</Button>
                 </Link>
               </span>
             </div>
