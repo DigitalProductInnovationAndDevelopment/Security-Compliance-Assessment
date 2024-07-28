c59e3c59cc7a003efaff6777586817c402d58743
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-07-28 15:06:34 +0200

    💄 refactor: Update AddNewProjectCard component to include a card header and description

--- src/components/addnewprojectcard.tsx
+++ src/components/addnewprojectcard.tsx
@@ -1,14 +1,28 @@
-'use client';
+"use client";
 import React from "react";
-import { Card, CardContent } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 import { PlusCircleIcon } from "lucide-react";
 import { Label } from "./ui/label";
 
 export default function AddNewProjectCard() {
   return (
-    <Card className="relative w-full max-w-sm bg-slate-100 sm:flex-1">
-      <CardContent className="relative h-full">
+    <Card className="relative w-full max-w-sm sm:flex-1">
+      <CardHeader className="bg-slate-100">
+        <CardTitle className="text-center text-2xl font-bold">
+          Add a new Project
+        </CardTitle>
+        <CardDescription className="text-center opacity-0">
+          Create a new project
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="relative h-[70%]">
         <div className="flex h-full flex-col items-center justify-center">
           <Tooltip>
             <TooltipTrigger onClick={() => alert("Add a new Project")}>
