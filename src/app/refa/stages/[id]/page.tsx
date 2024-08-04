f6bdb0f46465c10437c48c4a54327f6f5115cca0
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-08-04 18:26:48 +0200

    🎨 refactor: Extract AreaSheet component from Page component for better code organization

--- src/app/refa/stages/[id]/page.tsx
+++ src/app/refa/stages/[id]/page.tsx
@@ -11,6 +11,7 @@ import {
 import { Badge } from "~/components/ui/badge";
 import { Separator } from "~/components/ui/separator";
 import CardWrapper from "~/components/ui/cardWrapper";
+import { Area, Artefact } from "@prisma/client";
 
 export default async function Page({
   params,
@@ -25,91 +26,103 @@ export default async function Page({
     <div className="relative min-h-full w-full rounded-lg bg-white p-8">
       <div className="flex min-w-full flex-wrap gap-4">
         {areasWithArtefacts?.areas.map((area) => (
-          <Sheet key={area.area_id}>
+          <AreaSheet key={area.area_id} area={area}>
             <CardWrapper
               area={{ id: area.area_id, name: area.area_name }}
               artifacts={area.artefacts.map((artefact) => ({
                 id: artefact.artefact_id,
                 name: artefact.artefact_name,
               }))}
-              // SheetTrigger is in a Area Card now.
-              // TODO: Change the logic for sheet trigger and sheet content be in one file
-            ></CardWrapper>
-            <SheetContent className="lg:min-w-[40vw]">
-              <SheetHeader>
-                <SheetTitle className="text-center">
-                  <div>Area Details: </div>
-                  {area.area_name}
-                </SheetTitle>
-                <SheetDescription className="text-left">
-                  <div className="my-4 flex">
-                    <SheetTitle>People</SheetTitle>
-                    <Badge
-                      variant={
-                        area.people === "High"
-                          ? "destructive"
-                          : area.technology === "Medium"
-                            ? "secondary"
-                            : "default"
-                      }
-                      className="ml-2"
-                    >
-                      {area.people}
-                    </Badge>
-                  </div>
-                  <ul>
-                    {area.people_practices.map((practice) => (
-                      <li key={practice}>{practice}</li>
-                    ))}
-                  </ul>
-                  <Separator className="my-4" />
-                  <div className="my-4 flex">
-                    <SheetTitle>Process</SheetTitle>
-                    <Badge
-                      variant={
-                        area.process === "High"
-                          ? "destructive"
-                          : area.technology === "Medium"
-                            ? "secondary"
-                            : "default"
-                      }
-                      className="ml-2"
-                    >
-                      {area.process}
-                    </Badge>
-                  </div>
-                  <ul>
-                    {area.process_practices.map((practice) => (
-                      <li key={practice}>{practice}</li>
-                    ))}
-                  </ul>
-                  <Separator className="my-4" />
-                  <div className="my-4 flex">
-                    <SheetTitle>Technology</SheetTitle>
-                    <Badge
-                      variant={
-                        area.technology === "High"
-                          ? "destructive"
-                          : area.technology === "Medium"
-                            ? "secondary"
-                            : "default"
-                      }
-                      className="ml-2"
-                    >
-                      {area.technology}
-                    </Badge>
-                  </div>
-                  <ul>
-                    {area.technology_practices.map((practice) => (
-                      <li key={practice}>{practice}</li>
-                    ))}
-                  </ul>
-                </SheetDescription>
-              </SheetHeader>
-            </SheetContent>
-          </Sheet>
+            />
+          </AreaSheet>
         ))}
       </div>
     </div>
+  );
+}
+
+export function AreaSheet({
+  area,
+  children,
+}: Readonly<{
+  area: Area & { artefacts: Artefact[] };
+  children: React.ReactNode;
+}>) {
+  return (
+    <Sheet key={area.area_id}>
+      <SheetTrigger asChild>{children}</SheetTrigger>
+      <SheetContent className="lg:min-w-[40vw]">
+        <SheetHeader>
+          <SheetTitle className="text-center">
+            <div>Area Details: </div>
+            {area.area_name}
+          </SheetTitle>
+          <SheetDescription className="text-left">
+            <div className="my-4 flex">
+              <SheetTitle>People</SheetTitle>
+              <Badge
+                variant={
+                  area.people === "High"
+                    ? "destructive"
+                    : area.technology === "Medium"
+                      ? "secondary"
+                      : "default"
+                }
+                className="ml-2"
+              >
+                {area.people}
+              </Badge>
+            </div>
+            <ul>
+              {area.people_practices.map((practice) => (
+                <li key={practice}>{practice}</li>
+              ))}
+            </ul>
+            <Separator className="my-4" />
+            <div className="my-4 flex">
+              <SheetTitle>Process</SheetTitle>
+              <Badge
+                variant={
+                  area.process === "High"
+                    ? "destructive"
+                    : area.technology === "Medium"
+                      ? "secondary"
+                      : "default"
+                }
+                className="ml-2"
+              >
+                {area.process}
+              </Badge>
+            </div>
+            <ul>
+              {area.process_practices.map((practice) => (
+                <li key={practice}>{practice}</li>
+              ))}
+            </ul>
+            <Separator className="my-4" />
+            <div className="my-4 flex">
+              <SheetTitle>Technology</SheetTitle>
+              <Badge
+                variant={
+                  area.technology === "High"
+                    ? "destructive"
+                    : area.technology === "Medium"
+                      ? "secondary"
+                      : "default"
+                }
+                className="ml-2"
+              >
+                {area.technology}
+              </Badge>
+            </div>
+            <ul>
+              {area.technology_practices.map((practice) => (
+                <li key={practice}>{practice}</li>
+              ))}
+            </ul>
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
   );
 }
