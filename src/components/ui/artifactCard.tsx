a910b6e50e065524bdbb2344cf93881d9b8ea0a1
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-08-04 18:25:54 +0200

    🎨 refactor: Refactor ArtifactCard component to use ArtifactDialog for better code organization

--- src/components/ui/artifactCard.tsx
+++ src/components/ui/artifactCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-
 import { CardDescription, CardHeader } from "./card";
 import { Dialog, DialogTrigger, DialogContent, DialogTitle } from "./dialog";
 import { api } from "~/trpc/react";
@@ -14,12 +13,43 @@ export type ArtifactProps = {
   isEmpty?: boolean;
 };
 
+export const ArtifactDialog = ({
+  id,
+  name,
+  children,
+  className,
+}: ArtifactProps & { children: React.ReactNode } & { className?: string }) => {
+  const { data, isLoading } = api.refa.artefactById.useQuery(
+    id ? { artefact_id: id } : skipToken,
+  );
+
+  return (
+    <Dialog>
+      <DialogTrigger className={className}>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogTitle>{data?.artefact_name}</DialogTitle>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div className="p-3">
+            <div>{data?.description}</div>
+            <div className="py-3">
+              <span className="font-medium">Artefact Id</span>:{" "}
+              {data?.artefact_id}
+            </div>
+            <div>
+              <span className="font-medium">Automation</span>:{" "}
+              {data?.automation}
+            </div>
+          </div>
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
+
 const ArtifactCard = React.forwardRef<HTMLDivElement, ArtifactProps>(
   ({ id, name, isEmpty = false }, ref) => {
-    const { data, isLoading } = api.refa.artefactById.useQuery(
-      id ? { artefact_id: id } : skipToken,
-    );
-
     return (
       <>
         {isEmpty ? (
@@ -27,35 +57,14 @@ const ArtifactCard = React.forwardRef<HTMLDivElement, ArtifactProps>(
             <CardHeader>No Artifacts</CardHeader>
           </div>
         ) : (
-          <Dialog>
-            <DialogTrigger className="relative w-full rounded-xl border bg-card text-center text-card-foreground opacity-100 transition-opacity hover:opacity-50">
-              <div className="">
-                <CardHeader className="p-3">
-                  <span className="text-sm font-bold">{name}</span>
-                </CardHeader>
-
-                <CardDescription className="pb-2">{id}</CardDescription>
-              </div>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogTitle>{data?.artefact_name}</DialogTitle>
-              {isLoading ? (
-                <Spinner />
-              ) : (
-                <div className="p-3">
-                  <div>{data?.description}</div>
-                  <div className="py-3">
-                    <span className="font-medium">Artefact Id</span>:{" "}
-                    {data?.artefact_id}
-                  </div>
-                  <div>
-                    <span className="font-medium">Automation</span>:{" "}
-                    {data?.automation}
-                  </div>
-                </div>
-              )}
-            </DialogContent>
-          </Dialog>
+          <ArtifactDialog id={id} name={name} className="w-full">
+            <div className="rounded-xl border bg-card text-center text-card-foreground opacity-100 transition-opacity hover:opacity-50">
+              <CardHeader className="p-3">
+                <span className="text-sm font-bold">{name}</span>
+              </CardHeader>
+              <CardDescription className="pb-2">{id}</CardDescription>
+            </div>
+          </ArtifactDialog>
         )}
       </>
     );
