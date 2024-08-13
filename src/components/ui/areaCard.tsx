1ffbc66275887094cdbbe49dad0b1eb767194199
Nataliia Severynenko <49091101+severynenko@users.noreply.github.com>
2024-08-13 03:05:29 +0200

    fix: add separate loading logic

--- src/components/ui/areaCard.tsx
+++ src/components/ui/areaCard.tsx
@@ -15,6 +15,7 @@ import { useSession } from "next-auth/react";
 import { cn } from "~/lib/utils";
 import useProjectDetailsStore from "~/stores/useProjectDetailsStore";
 import { api } from "~/trpc/react";
+import { useEffect, useState } from "react";
 
 export interface AreaProps {
   id: string;
@@ -26,22 +27,25 @@ export interface AreaProps {
 const AreaCard = React.forwardRef<HTMLDivElement, AreaProps>((area, ref) => {
   const { data: session } = useSession();
   const userLoggedIn = !!session;
+  const [loading, setLoading] = useState(true);
 
-  const { data: areaDetails } = api.refa.areaByAreaId.useQuery({
-    area_id: area.id,
-  });
+  const { data: areaDetails, isFetched: isAreaFetched } =
+    api.refa.areaByAreaId.useQuery({
+      area_id: area.id,
+    });
 
   const { currentProject } = useProjectDetailsStore();
 
-  const { data: areaScore } = api.assessment.getAreaScore.useQuery(
-    {
-      projectId: currentProject?.id || -1,
-      areaId: areaDetails?.id || -1,
-    },
-    {
-      enabled: userLoggedIn && !!currentProject?.id && !!areaDetails?.id,
-    },
-  );
+  const { data: areaScore, isFetched: isScoreFetched } =
+    api.assessment.getAreaScore.useQuery(
+      {
+        projectId: currentProject?.id || -1,
+        areaId: areaDetails?.id || -1,
+      },
+      {
+        enabled: userLoggedIn && !!currentProject?.id && !!areaDetails?.id,
+      },
+    );
 
   // Default values if areaScore is undefined
   const areaMaturityScore = areaScore?.areaMaturityScore ?? 0;
@@ -66,74 +70,88 @@ const AreaCard = React.forwardRef<HTMLDivElement, AreaProps>((area, ref) => {
                 : ""
     : ""; // if not loggedIn show no color coding
 
+  useEffect(() => {
+    if (isAreaFetched && isScoreFetched) {
+      setLoading(false);
+    }
+  }, [isAreaFetched, isScoreFetched]);
+
   return (
-    <Card
-      title={area.name}
-      className={cn(
-        "flex h-full w-full flex-col items-center justify-center",
-        backgroundColor,
-      )}
-    >
-      <div className="absolute right-2 top-2 space-x-1">
-        <Button
-          variant="outline"
-          size="icon"
-          className="h-4 w-4 shrink-0 rounded-full opacity-50 hover:bg-accent hover:text-accent-foreground"
-          onClick={area.toggleVisibility}
+    <>
+      {loading ? (
+        <div className="flex animate-pulse items-center justify-center p-4">
+          <div className="h-16 w-full max-w-md rounded bg-gray-100"></div>
+        </div>
+      ) : (
+        <Card
+          title={area.name}
+          className={cn(
+            "flex h-full w-full flex-col items-center justify-center",
+            backgroundColor,
+          )}
         >
-          {area.visible ? <Icons.show /> : <Icons.hide />}
-        </Button>
-        {/* Info icon to trigger info sheet */}
-        <SheetTrigger>
-          <div className="h-4 w-4 shrink-0 rounded-full opacity-50 hover:bg-accent hover:text-accent-foreground">
-            <Icons.info />
-          </div>
-        </SheetTrigger>
-        {/* If user is logged in, edit icon to trigger assess sheet */}
-        {userLoggedIn && (
-          <Sheet>
+          <div className="absolute right-2 top-2 space-x-1">
+            <Button
+              variant="outline"
+              size="icon"
+              className="h-4 w-4 shrink-0 rounded-full opacity-50 hover:bg-accent hover:text-accent-foreground"
+              onClick={area.toggleVisibility}
+            >
+              {area.visible ? <Icons.show /> : <Icons.hide />}
+            </Button>
+            {/* Info icon to trigger info sheet */}
             <SheetTrigger>
               <div className="h-4 w-4 shrink-0 rounded-full opacity-50 hover:bg-accent hover:text-accent-foreground">
-                <Icons.edit />
+                <Icons.info />
               </div>
             </SheetTrigger>
-            <SheetContent className="lg:min-w-[40vw]">
-              <SheetHeader>
-                <SheetTitle className="text-center">
-                  <div>Assess Area: </div>
-                  {area.name}
-                </SheetTitle>
-              </SheetHeader>
-              <div className="mt-4">
-                <p>TODO: Edit assessment about this area.</p>
-                {/* TODO: Implement assessment component here */}
-              </div>
-            </SheetContent>
-          </Sheet>
-        )}
-      </div>
-      <div
-        className={cn(
-          "text-bold absolute bottom-2 right-2 space-x-1 text-sm",
-          backgroundColor,
-        )}
-      >
-        {maturityScore.toFixed(0)}%
-      </div>
-      <CardHeader
-        className="flex flex-col items-center justify-center"
-        title={area.name}
-      >
-        <CardTitle className="text-center">{area.name}</CardTitle>
-      </CardHeader>
-      <CardContent className="flex flex-col items-center justify-center">
-        <div className="text-center">
-          <p className={cn("text-sm text-gray-500", backgroundColor)}>
-            {area.id}
-          </p>
-        </div>
-      </CardContent>
-    </Card>
+            {/* If user is logged in, edit icon to trigger assess sheet */}
+            {userLoggedIn && (
+              <Sheet>
+                <SheetTrigger>
+                  <div className="h-4 w-4 shrink-0 rounded-full opacity-50 hover:bg-accent hover:text-accent-foreground">
+                    <Icons.edit />
+                  </div>
+                </SheetTrigger>
+                <SheetContent className="lg:min-w-[40vw]">
+                  <SheetHeader>
+                    <SheetTitle className="text-center">
+                      <div>Assess Area: </div>
+                      {area.name}
+                    </SheetTitle>
+                  </SheetHeader>
+                  <div className="mt-4">
+                    <p>TODO: Edit assessment about this area.</p>
+                    {/* TODO: Implement assessment component here */}
+                  </div>
+                </SheetContent>
+              </Sheet>
+            )}
+          </div>
+          <div
+            className={cn(
+              "text-bold absolute bottom-2 right-2 space-x-1 text-sm",
+              backgroundColor,
+            )}
+          >
+            {maturityScore.toFixed(0)}%
+          </div>
+          <CardHeader
+            className="flex flex-col items-center justify-center"
+            title={area.name}
+          >
+            <CardTitle className="text-center">{area.name}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center justify-center">
+            <div className="text-center">
+              <p className={cn("text-sm text-gray-500", backgroundColor)}>
+                {area.id}
+              </p>
+            </div>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 });
 AreaCard.displayName = "AreaCard";
