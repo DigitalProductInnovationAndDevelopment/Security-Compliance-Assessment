6c0c7f1045594d05e8f5e468ad3d385055e92f1e
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2024-08-14 10:54:01 +0200

    ♻️ refactor: Update Home page to redirect to '/refa/stages/1'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default async function Home() {
-  // WIP: This is just a placeholder to show how the logic of the Home page can be implemented
-  // const {data, isLoading, error} = api.query.projects()
-
-  // if (isLoading) return <div>Loading...</div>
-
-  // if (error) return <div>Error: {error.message}</div>
-
-  // if (!data.length) {
-  //   redirect('/refa')
-  // } else {
-  //   redirect('/projects')
-  // }
-
-  return (
-    <div className="flex min-h-screen w-full flex-col px-6">
-      Home
-    </div>
-  );
+  redirect('/refa/stages/1')
 }
